64fa74f9158974b7dbc9fca676d95ea5bcdd7785
Amare Ushule <amirushule@gmail.com>
2025-12-16 09:21:26 +0300

    Set up a GraphQL endpoint and define your first schema and query.

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,12 @@
 import graphene
 
-class Query(graphene.ObjectType):
+
+class CRMQuery:
     hello = graphene.String(default_value="Hello, GraphQL!")
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
 
 schema = graphene.Schema(query=Query)
